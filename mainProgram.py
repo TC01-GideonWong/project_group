81e82b44dabd788beac2cd2643843cc7f9497d84
jaimietohh <124014820+jaimietohh@users.noreply.github.com>
2023-02-02 13:58:34 +0800

    Create mainProgram.py

---
+++ mainProgram.py
@@ -0,0 +1,15 @@
+#importing all the other python files
+from overheads import*
+from cash_on_hand import*
+from profit_loss import*
+
+def main program():
+  '''
+  This function has no parameters and will run all the other functions from the imported python files
+  '''
+  overheads_highest()
+  coh_difference()
+  pl_difference()
+  
+#running main program
+(main_program())
