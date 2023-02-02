7bd3712f4f8c966e947687b48864bc0eb37aad3f
jaimietohh <124014820+jaimietohh@users.noreply.github.com>
2023-02-02 13:41:09 +0800

    Update main_program.py

--- main_program.py
+++ main_program.py
@@ -4,7 +4,7 @@ from profit_loss import*
 
 def main_program():
   '''
-  this function has no parameters and will run all the other functions
+  this function has no parameters and will run all the other functions from the imported python files
   '''
   overheads_highest()
   coh_difference()
